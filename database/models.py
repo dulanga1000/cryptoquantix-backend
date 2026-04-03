076d2ab1ac0550d1a3c85ad09414954545aed0d1
Oshana Kavinash 0743563623 <oshanachamuditha@gmail.com>
2026-04-03 13:02:08 +0530

    DB models, migrations, seed, health APIs

---
+++ database/models.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+
+# 👤 USER TABLE
+# -----------------------------
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
